dc1e479cb906775adf6eec85e53d366a19edc5a0
Michal.Wasniowski <Michal.Wasniowski@1b16859e-760e-11de-ae9c-95a02ec14c43>
2009-07-21 16:29:28 +0000

    Video aplikacja strumieniajaca i odtwarzajaca filmy

---
+++ trunk/VideoClient/src/pl/edu/agh/ims/video/client/PlatformAdapter.java
@@ -0,0 +1,28 @@
+package pl.edu.agh.ims.video.client;
+
+import com.ericsson.icp.IPlatformListener;
+import com.ericsson.icp.util.ErrorReason;
+
+public class PlatformAdapter implements IPlatformListener {
+
+	public void processApplicationData(String arg0, byte[] arg1, int arg2) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void processIncomingProfile(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void processPlatformTerminated(ErrorReason arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void processError(ErrorReason arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ trunk/VideoClient/src/pl/edu/agh/ims/video/client/VideoClientGui.java
@@ -0,0 +1,89 @@
+package pl.edu.agh.ims.video.client;
+
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+public class VideoClientGui extends JPanel {
+
+	private static final long serialVersionUID = 3146874614664014160L;
+
+	static JButton choseMovieButton;
+
+	static JComboBox moviesComboBox;
+
+	VideoClient client = new VideoClient();
+
+	public static void setMoviesList(String[] moviesStrings) {
+		choseMovieButton.setEnabled(true);
+		for (String movie : moviesStrings) {
+			moviesComboBox.addItem(movie);
+		}
+		moviesComboBox.setEnabled(true);
+	}
+
+	public VideoClientGui() {
+		super(new BorderLayout());
+
+		JButton startButton = new JButton("Start");
+		startButton.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent arg0) {
+				client.getMovieList();
+
+			}
+
+		});
+
+		choseMovieButton = new JButton("GO");
+		choseMovieButton.setEnabled(false);
+		choseMovieButton.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				String movie = (String) moviesComboBox.getSelectedItem();
+				client.showChosenMovie(movie);
+
+			}
+		});
+
+		add(startButton, BorderLayout.PAGE_START);
+
+		moviesComboBox = new JComboBox();
+
+		moviesComboBox.setPreferredSize(new Dimension(200, 40));
+		moviesComboBox.setEnabled(false);
+
+		add(moviesComboBox, BorderLayout.CENTER);
+		add(choseMovieButton, BorderLayout.PAGE_END);
+	}
+
+	/**
+	 * Create the GUI and show it. For thread safety, this method should be
+	 * invoked from the event-dispatching thread.
+	 */
+	static void createAndShowGUI() {
+		// Create and set up the window.
+		JFrame frame = new JFrame("Video Streaming");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		// Create and set up the content pane.
+		JComponent newContentPane = new VideoClientGui();
+		newContentPane.setOpaque(true); // content panes must be opaque
+		frame.setContentPane(newContentPane);
+
+		// Display the window.
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		// Schedule a job for the event-dispatching thread:
+		// creating and showing this application's GUI.
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGUI();
+			}
+		});
+	}
+}
