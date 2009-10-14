5ec4ab14e0eaabf020bf7d3793b8497376fd86e0
ewozniakow <ewozniakow@1b16859e-760e-11de-ae9c-95a02ec14c43>
2009-10-14 16:08:05 +0000

    Initial commit.

---
+++ trunk/HelloWorldClient/src/com/ericsson/sds/samples/helloworld/HelloWorldMsg.java
@@ -0,0 +1,16 @@
+package com.ericsson.sds.samples.helloworld;
+
+
+public class HelloWorldMsg {
+
+	private String msg;
+	
+	public HelloWorldMsg(String msg){
+		this.msg = msg;
+	}
+	
+	public String getMsg(){
+		return this.msg;
+	}
+	
+}
