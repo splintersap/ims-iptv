566357aaa5b0cfea1b77626e9c40dfcfba7ec762
michal.wasniowski <michal.wasniowski@1b16859e-760e-11de-ae9c-95a02ec14c43>
2009-10-22 13:48:40 +0000

    client working with new server

--- trunk/VideoClient/src/pl/edu/agh/ims/video/client/ServiceAdapter.java
+++ trunk/VideoClient/src/pl/edu/agh/ims/video/client/ServiceAdapter.java
@@ -7,96 +7,88 @@ import com.ericsson.icp.util.ErrorReason;
 public class ServiceAdapter implements IServiceListener {
 
 	public void processIncomingSession(ISession arg0) {
-		// TODO Auto-generated method stub
+		System.out.println("processIncomingSession");
 
 	}
 
 	public void processMessage(String arg0, String arg1, byte[] arg2, int arg3) {
-		// TODO Auto-generated method stub
+		System.out.println("processMessage");
 
 	}
 
 	public void processMessage(String arg0, String arg1, byte[] arg2, int arg3,
 			String arg4) {
-		// TODO Auto-generated method stub
+		System.out.println("processMessage2");
 
 	}
 
 	public void processOptions(String arg0, String arg1, String arg2,
 			byte[] arg3, int arg4) {
-		// TODO Auto-generated method stub
+		System.out.println("processOptions");
 
 	}
 
 	public void processPublishResult(boolean arg0, String arg1, String arg2,
 			long arg3) {
-		// TODO Auto-generated method stub
-
+		System.out.println("processPublishResult");
 	}
 
 	public void processRefer(String arg0, String arg1, String arg2,
 			String arg3, byte[] arg4, int arg5) {
-		// TODO Auto-generated method stub
-
+		System.out.println("processRefer");
 	}
 
 	public void processReferEnded(String arg0) {
-		// TODO Auto-generated method stub
+		System.out.println("processReferEnded");
 
 	}
 
 	public void processReferNotification(String arg0, int arg1) {
-		// TODO Auto-generated method stub
+		System.out.println("processReferNotification");
 
 	}
 
 	public void processReferNotifyResult(boolean arg0, String arg1, long arg2) {
-		// TODO Auto-generated method stub
+		System.out.println("processReferNotifyResult");
 
 	}
 
 	public void processReferResult(boolean arg0, String arg1, long arg2) {
-		// TODO Auto-generated method stub
-
+		System.out.println("processReferResult");
 	}
 
 	public void processSendMessageResult(boolean arg0, long arg1) {
-		// TODO Auto-generated method stub
-
+		System.out.println("processSendMessageResult");
 	}
 
 	public void processSendOptionsResult(boolean arg0, String arg1,
 			String arg2, String arg3, byte[] arg4, int arg5, long arg6) {
-		// TODO Auto-generated method stub
-
+		System.out.println("processSendOptionsResult");
 	}
 
 	public void processSubscribeEnded(String arg0, String arg1, String arg2) {
-		// TODO Auto-generated method stub
+		System.out.println("processSubscribeEnded");
 
 	}
 
 	public void processSubscribeNotification(String arg0, String arg1,
 			String arg2, String arg3, byte[] arg4, int arg5) {
-		// TODO Auto-generated method stub
-
+		System.out.println("processSubscribeNotification");
 	}
 
 	public void processSubscribeResult(boolean arg0, String arg1, String arg2,
 			long arg3) {
-		// TODO Auto-generated method stub
-
+		System.out.println("processSubscribeResult");
 	}
 
 	public void processUnsubscribeResult(boolean arg0, String arg1,
 			String arg2, long arg3) {
-		// TODO Auto-generated method stub
+		System.out.println("processUnsubscribeResult");
 
 	}
 
 	public void processError(ErrorReason arg0) {
-		// TODO Auto-generated method stub
-
+		System.out.println("processError");
 	}
 
 }
