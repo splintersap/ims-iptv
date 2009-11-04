2d9a182049481cb22b3bc5007137191c601d26e6
ewozniakow <ewozniakow@1b16859e-760e-11de-ae9c-95a02ec14c43>
2009-11-04 10:01:54 +0000

    Added iperf.exe.

--- trunk/NetworkPerformanceClient/src/pl/edu/agh/performance/client/core/PerformanceMeasurment.java
+++ trunk/NetworkPerformanceClient/src/pl/edu/agh/performance/client/core/PerformanceMeasurment.java
@@ -12,7 +12,7 @@ public class PerformanceMeasurment {
 
 	public PerformanceMeasurment(String serverAddress, int port, int bandwidth,
 			int frameSize) {
-		options = new String("bin/iperf.exe -c " + serverAddress
+		options = new String("iperf/iperf.exe -c " + serverAddress
 				+ " -u -i 1 -p " + port + " -b " + bandwidth
 				+ "M -t 10 -T 255 -l " + frameSize + " -r");
 		performQuery();
