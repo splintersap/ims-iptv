32636285108210c18b460629e0e96e12e2f4b5a7
ewozniakow <ewozniakow@1b16859e-760e-11de-ae9c-95a02ec14c43>
2009-11-04 16:31:42 +0000

    Version which closes iperf.exe when user closes the program main window.

---
+++ trunk/IPTVClient/src/pl/edu/agh/iptv/listeners/IperfManagerListener.java
@@ -0,0 +1,61 @@
+package pl.edu.agh.iptv.listeners;
+
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import pl.edu.agh.performance.client.core.IperfThread;
+
+public class IperfManagerListener implements WindowListener{
+
+	private IperfThread iperfThread;
+	
+	
+	public IperfManagerListener(IperfThread iperfThread){
+		this.iperfThread = iperfThread;
+	}
+	
+	@Override
+	public void windowActivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		// TODO Auto-generated method stub
+		iperfThread.quit();
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowOpened(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}
