78951c5b9e26223881e3d799ad989db6ea7bec76
michal.wasniowski <michal.wasniowski@1b16859e-760e-11de-ae9c-95a02ec14c43>
2009-11-05 09:29:07 +0000

    OK send to INFO

--- IPTVServlet/src/pl/edu/agh/iptv/servlet/VideoServlet.java
+++ IPTVServlet/src/pl/edu/agh/iptv/servlet/VideoServlet.java
@@ -8,15 +8,20 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import javax.servlet.ServletException;
-import javax.servlet.sip.SipFactory;
 import javax.servlet.sip.SipServlet;
 import javax.servlet.sip.SipServletRequest;
 import javax.servlet.sip.SipServletResponse;
 import javax.servlet.sip.SipSession;
 import javax.transaction.UserTransaction;
 
+import net.sourceforge.jsdp.Information;
+import net.sourceforge.jsdp.Origin;
+import net.sourceforge.jsdp.SDPException;
+import net.sourceforge.jsdp.SDPFactory;
+import net.sourceforge.jsdp.SessionDescription;
 import pl.edu.agh.iptv.servlet.facade.MessageCreator;
 import pl.edu.agh.iptv.servlet.persistence.Movie;
+import pl.edu.agh.iptv.servlet.persistence.User;
 
 @javax.servlet.sip.annotation.SipServlet
 public class VideoServlet extends SipServlet {
@@ -55,12 +60,51 @@ public class VideoServlet extends SipServlet {
 		Movie movie = movieList.get(0);
 		return movie.getMoviePath();
 	}
-	
+
+	private void setRatingToDatabase(String title, Integer rating, String sip) {
+		try {
+			utx.begin();
+			Query query = em.createQuery("FROM Movie WHERE title = '" + title
+					+ "'");
+			List<Movie> movieList = query.getResultList();
+			Movie movie = movieList.get(0);
+			log("Movie = " + movie);
+
+			Query userQuery = em.createQuery("FROM User WHERE sip = '" + sip
+					+ "'");
+			List<User> userList = userQuery.getResultList();
+			User user = userList.get(0);
+			log("User = " + user);
+
+			log("Updating rating");
+			movie.addMovieRating(user, rating);
+
+			utx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
 	@Override
 	protected void doInfo(SipServletRequest req) throws ServletException,
 			IOException {
-		log("Wiadomosc doInfo " + req);
 		
+		req.createResponse(200).send();
+
+		String contentType = req.getContentType();
+		String[] tab = contentType.split("/");
+
+		if (tab.length == 2 && "rating".equals(tab[0])) {
+			String title = tab[1];
+			Integer rating = new Integer(new String(req.getRawContent()));
+			setRatingToDatabase(title, rating, req.getFrom().getURI()
+					.toString());
+			log("Rating updated : title = " + title + ", rating = " + rating
+					+ ", sip=" + req.getFrom().getURI().toString());
+		} else {
+			log("Wrong info message " + req);
+		}
 	}
 
 	/**
@@ -70,6 +114,7 @@ public class VideoServlet extends SipServlet {
 			throws ServletException, IOException {
 
 		log("inside doAck method");
+		createSDPFromMovie(null);
 		SipSession session = sipServletRequest.getSession(true);
 		// Ensure we do not process the ACK twice for the dialog
 		Object ackAlreadyProcessed = session.getAttribute(ACK_RECEIVED);
@@ -81,8 +126,8 @@ public class VideoServlet extends SipServlet {
 			SipServletRequest message = session.createRequest("MESSAGE");
 
 			try {
-				String xml = MessageCreator.getMessage(em, utx, sipServletRequest
-						.getFrom().getURI().toString());
+				String xml = MessageCreator.getMessage(em, utx,
+						sipServletRequest.getFrom().getURI().toString());
 				message.setContent(xml, "text/movie-list");
 
 			} catch (Exception e) {
@@ -91,8 +136,43 @@ public class VideoServlet extends SipServlet {
 			message.send();
 		}
 	}
-	
-	
+
+	private SessionDescription createSDPFromMovie(Movie movie) {
+		SessionDescription sessionDescription = null;
+		try {
+			sessionDescription = SDPFactory.createSessionDescription();
+		} catch (SDPException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		try {
+			sessionDescription.setVersion(SDPFactory.createVersion());
+			Origin orgin = SDPFactory.createOrigin();
+			sessionDescription.setOrigin(orgin);
+
+		} catch (SDPException e) {
+			e.printStackTrace();
+		}
+
+		if (movie != null) {
+			try {
+				Information info = SDPFactory.createInformation(movie.getTitle() + ";"
+						+ movie.getDirector() + ";" + movie.getCategory());
+				sessionDescription.setInformation(info);
+				SDPFactory.createAttribute("description", movie.getDescription());
+			} catch (SDPException e) {
+				
+				e.printStackTrace();
+			}
+
+		}
+
+		log(sessionDescription.toString());
+
+		return sessionDescription;
+
+	}
 
 	/**
 	 * @inheritDoc
