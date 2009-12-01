4d7f42842f481a8c120f79b04b2625edb5e6efdd
ewozniakow <ewozniakow@1b16859e-760e-11de-ae9c-95a02ec14c43>
2009-12-01 16:54:15 +0000

    Some updates of common movie watching.

--- IPTVClient/src/pl/edu/agh/iptv/view/CommonWatchingView.java
+++ IPTVClient/src/pl/edu/agh/iptv/view/CommonWatchingView.java
@@ -1,6 +1,7 @@
 package pl.edu.agh.iptv.view;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 
@@ -14,6 +15,7 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 
 import pl.edu.agh.iptv.view.components.MenuCellRenderer;
+import pl.edu.agh.iptv.view.components.MyCalendar;
 import pl.edu.agh.iptv.view.components.ViewWithCalendar;
 import pl.edu.agh.iptv.view.movies.MoviesTab;
 
@@ -31,12 +33,18 @@ public class CommonWatchingView extends ViewWithCalendar {
 	private JTextField yearS = new JTextField(4);
 	private JTextField hourS = new JTextField(2);
 	private JTextField minuteS = new JTextField(2);
+	
+	private JButton okButton = new JButton("OK");
+	private JButton cancelButton = new JButton("CANCEL");
+	
+	private MyCalendar myCalendar;
 
 	JCalendar calendar = new JCalendar();
 
 	public CommonWatchingView(JFrame mainFrame, MoviesTab moviesTab,
 			String selected) {
 		super(mainFrame);
+		this.setTitle("Common watching");
 		JList moviesList = new JList(moviesTab.getAllMovies().toArray());
 		moviesList.setCellRenderer(new MenuCellRenderer());
 		JScrollPane moviesSC = new JScrollPane(moviesList);
@@ -45,7 +53,7 @@ public class CommonWatchingView extends ViewWithCalendar {
 		descPane.setLayout(new BorderLayout());
 
 		JPanel sDesc = new JPanel(new BorderLayout());
-		sDesc.add(new JLabel("Common watching"), BorderLayout.NORTH);
+		sDesc.setPreferredSize(new Dimension(100, 70));		
 		sDesc.add(new JLabel("Users to invite: " + selected),
 				BorderLayout.CENTER);
 
@@ -79,8 +87,15 @@ public class CommonWatchingView extends ViewWithCalendar {
 		datePanel.add(startP1, BorderLayout.NORTH);
 		datePanel.add(startP2, BorderLayout.CENTER);
 
+		JPanel buttonsP = new JPanel(new FlowLayout());
+		okButton.setName("COMMON_OK");
+		cancelButton.setName("COMMON_CANCEL");
+		buttonsP.add(okButton);
+		buttonsP.add(cancelButton);
+		
 		descPane.add(sDesc, BorderLayout.NORTH);
 		descPane.add(datePanel, BorderLayout.CENTER);
+		descPane.add(buttonsP, BorderLayout.SOUTH);			
 
 		this.add(moviesSC, BorderLayout.WEST);
 		this.add(descPane, BorderLayout.EAST);
@@ -93,7 +108,26 @@ public class CommonWatchingView extends ViewWithCalendar {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		if (((JButton) e.getSource()).getName().compareTo("START") == 0) {
+			myCalendar = new MyCalendar(calendar, this);
+		} else if (((JButton) e.getSource()).getName().compareTo("CALENDAR_OK") == 0) {
 
+				Integer day = new Integer(calendar.getDayChooser().getDay());
+				this.dayS.setText(day.toString());
+
+				Integer month = new Integer(calendar.getMonthChooser()
+						.getMonth() + 1);
+				this.monthS.setText(month.toString());
+
+				Integer year = new Integer(calendar.getYearChooser().getYear());
+				this.yearS.setText(year.toString());
+
+				myCalendar.dispose();
+
+		} else if (((JButton) e.getSource()).getName().compareTo(
+				"CALENDAR_CANCEL") == 0) {
+			myCalendar.dispose();
+		}
 	}
 
 }
