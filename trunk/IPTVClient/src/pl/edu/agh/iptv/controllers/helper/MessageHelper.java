ed741c494f95e8605011065a6f11951c1cb63107
michal.wasniowski <michal.wasniowski@1b16859e-760e-11de-ae9c-95a02ec14c43>
2009-12-08 10:42:33 +0000

    pause and stoping is working

--- trunk/IPTVClient/src/pl/edu/agh/iptv/controllers/helper/MessageHelper.java
+++ trunk/IPTVClient/src/pl/edu/agh/iptv/controllers/helper/MessageHelper.java
@@ -34,70 +34,83 @@ public class MessageHelper {
 		try {
 			SessionDescription sdp = SDPFactory
 					.parseSessionDescription(message);
-			String description;
-			if (sdp.getAttribute("description") != null)
-				description = sdp.getAttribute("description").getValue();
-			else
-				description = "";
+			
 			String title = sdp.getInformation().getValue();
-			String category = sdp.getAttribute("category").getValue();
-			String director = sdp.getAttribute("director").getValue();
-			String userRating = sdp.getAttribute("userRating").getValue();
-			String overallRating = sdp.getAttribute("overallRating").getValue();
+			String description = getSDPAttribute(sdp, "description");
+			String category = getSDPAttribute(sdp, "category");
+			String director = getSDPAttribute(sdp, "director");
+			String userRating = getSDPAttribute(sdp, "userRating");
+			String overallRating = getSDPAttribute(sdp,"overallRating");
 
 			movie = new Movie(title, category, description, director, Integer
 					.valueOf(userRating), Double.valueOf(overallRating));
-			Attribute[] commentAtr = sdp.getAttributes("comment");
-			System.out.println("length = " + commentAtr.length);
-			for (Attribute atr : commentAtr) {
-				String comment = atr.getValue();
-				System.out.println(comment);
-				String[] strings = comment.split("\\|");
+			getMovieComments(movie, sdp);
 
-				if (strings.length == 3) {
-					String sip = strings[0];
-
-					Date date = new Date(Long.valueOf(strings[1]));
-
-					String com = strings[2];
-					Comment commonComment = new Comment(date, com, sip);
-					System.out.println(date + ", " + com + ", " + sip);
-					movie.addCommonComment(commonComment);
-
-				}
-			}
-
-			Attribute[] paymentAtr = sdp.getAttributes("payment");
-			for (Attribute atr : paymentAtr) {
-				String payment = atr.getValue();
-				System.out.println(payment);
-				String[] strings = payment.split("\\|");
-				if (strings.length == 3) {
-
-					Date date = null;
-					boolean isOrdered = false;
-					String quality = strings[0];
-					// cmd.setQuality(quality);
-					Long price = new Long(strings[1]);
-					// cmd.setPrice(price);
-					if ("null".equals(strings[2])) {
-						isOrdered = false;
-					} else {
-						date = new Date(new Long(strings[2]));
-						isOrdered = true;
-					}
-
-					MovieDescription movieDescription = new MovieDescription(
-							quality, price, date, isOrdered);
-					movie.addCommonMovieDescription(movieDescription);
-				}
-			}
+			getMoviePayments(movie, sdp);
 		} catch (SDPParseException e) {
 			e.printStackTrace();
 		}
 
 		return movie;
 
+	}
+
+	private void getMoviePayments(Movie movie, SessionDescription sdp) {
+		Attribute[] paymentAtr = sdp.getAttributes("payment");
+		for (Attribute atr : paymentAtr) {
+			String payment = atr.getValue();
+			System.out.println(payment);
+			String[] strings = payment.split("\\|");
+			if (strings.length == 3) {
+
+				Date date = null;
+				boolean isOrdered = false;
+				String quality = strings[0];
+				// cmd.setQuality(quality);
+				Long price = new Long(strings[1]);
+				// cmd.setPrice(price);
+				if ("null".equals(strings[2])) {
+					isOrdered = false;
+				} else {
+					date = new Date(new Long(strings[2]));
+					isOrdered = true;
+				}
+
+				MovieDescription movieDescription = new MovieDescription(
+						quality, price, date, isOrdered);
+				movie.addCommonMovieDescription(movieDescription);
+			}
+		}
+	}
+
+	private void getMovieComments(Movie movie, SessionDescription sdp) {
+		Attribute[] commentAtr = sdp.getAttributes("comment");
+		System.out.println("length = " + commentAtr.length);
+		for (Attribute atr : commentAtr) {
+			String comment = atr.getValue();
+			System.out.println(comment);
+			String[] strings = comment.split("\\|");
+
+			if (strings.length == 3) {
+				String sip = strings[0];
+
+				Date date = new Date(Long.valueOf(strings[1]));
+
+				String com = strings[2];
+				Comment commonComment = new Comment(date, com, sip);
+				System.out.println(date + ", " + com + ", " + sip);
+				movie.addCommonComment(commonComment);
+
+			}
+		}
+	}
+	
+	private String getSDPAttribute(SessionDescription sdp , String attribute) {
+		Attribute atr = sdp.getAttribute(attribute);
+		if(atr == null) {
+			return "";
+		}
+		return atr.getValue();
 	}
 
 }
--- trunk/IPTVClient/src/pl/edu/agh/iptv/listeners/PlayListener.java
+++ trunk/IPTVClient/src/pl/edu/agh/iptv/listeners/PlayListener.java
@@ -6,6 +6,8 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JList;
 
+import org.videolan.jvlc.VLCException;
+
 import pl.edu.agh.iptv.IPTVClient;
 import pl.edu.agh.iptv.controllers.helper.VLCHelper;
 import pl.edu.agh.iptv.view.MainView;
@@ -34,7 +36,11 @@ public class PlayListener implements ActionListener {
 			if(MainView.getPlayButton().getIcon().equals(MainView.playIcon))
 			{				
 				if(VLCHelper.isPlayingMovie) {
-					VLCHelper.mp.pause();
+					try {
+						VLCHelper.playlist.togglePause();
+					} catch (VLCException e1) {
+						e1.printStackTrace();
+					}
 				} else {
 					MenuListItem menuItem = (MenuListItem)value;
 					iptvClient.showChosenMovie(menuItem.getTitle());
@@ -44,10 +50,13 @@ public class PlayListener implements ActionListener {
 				isPlayButton = false;
 				
 			} else {
-				if(VLCHelper.mp != null)
-				{
-					VLCHelper.mp.pause();
+				
+				try {
+					VLCHelper.playlist.togglePause();
+				} catch (VLCException e1) {
+					e1.printStackTrace();
 				}
+				
 				isPlayButton = true;
 				button.setIcon(MainView.playIcon);
 			}
